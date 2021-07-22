3f8d75c2e39aed5fa12ca55846123b1a77a0e634
Nhữ Mạnh Cần <maccervn@gmail.com>
2021-07-22 16:26:04 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
-// import TodoListAdd from './components/TodoListAdd';
-// import TodoListItem from './components/TodoListItem';
 import TodoList from './components/TodoList';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <div className="col-6 col-sm-4 col-md-6 col-lg-3">
-//           <TodoListAdd />
-//           <TodoListItem />
-//         {/* <button type="button" className="btn btn-primary">Primary</button> */}
-//         </div>
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 function App() {
-  const item = [{
-    id: Date.now(),
-    todo: "a",
-    isCheck: true,
-  },
-  {
-    id: Date.now(),
-    todo: "b",
-    isCheck: false,
-  },
-]
   return (
-    <TodoList props={item}/>
+    <TodoList/>
   );
 }
 
